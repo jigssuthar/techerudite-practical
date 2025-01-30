2a27514c6b4e73d053b3f99706c72eab24e9a43a
jigssuthar <jigssuthar7861@gmail.com>
2025-01-30 15:48:49 +0530

    r

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Auth\CustomerRegisterController;
+use App\Http\Controllers\Auth\AdminLoginController;
+use App\Http\Controllers\Auth\AdminRegisterController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -29,15 +31,14 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
-use App\Http\Controllers\Auth\CustomerRegisterController;
-use App\Http\Controllers\Auth\AdminRegisterController;
+
 
 Route::get('customer-register', [CustomerRegisterController::class, 'showRegistrationForm'])->name('customer.register');
 Route::post('customer-register', [CustomerRegisterController::class, 'register']);
 
 Route::get('admin-register', [AdminRegisterController::class, 'showRegistrationForm'])->name('admin.register');
 Route::post('admin-register', [AdminRegisterController::class, 'register']);
-use App\Http\Controllers\Auth\AdminLoginController;
+
 
 Route::get('admin/login', [AdminLoginController::class, 'showLoginForm'])->name('admin.login');
 Route::post('admin/login', [AdminLoginController::class, 'login']);
